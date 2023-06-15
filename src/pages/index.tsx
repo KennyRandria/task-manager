a9c303410a2ae4e9e5af2d0574ba8e543bd19765
kenny <hei.kenny.3@gmail.com>
2023-06-15 12:07:53 +0300

    fix :serveur et difference horaire

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,55 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import { useState } from "react";
+import { GetServerSideProps } from "next";
 
-import { useEffect, useState } from "react";
+interface HomeProps {
+  serverTime: Date;
+}
 
-const calculateTimeDifference = (server, client) => {
-  const timeDiff = Math.abs(server - client);
-  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+/**
+  Calculates the time difference between the server time and client time.
+  @param {Date} serverTime - The server time.
+  @param {Date} clientTime - The client time.
+  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
+*/
+const calculateTimeDifference = (serverTime: Date, clientTime: Date): string => {
+  //Get the timestamps for each date
+  const timestampServer = serverTime.getTime();
+  const timestampClient = clientTime.getTime();
 
-  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  // Calculate the time difference in milliseconds
+  const timeDiff = Math.abs(timestampClient - timestampServer);
+
+  // Calculate the individual time components
+  const secondsDiff = Math.floor(timeDiff / 1000) % 60;
+  const minutesDiff = Math.floor(timeDiff / (1000 * 60)) % 60;
+  const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60)) % 24;
+  const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  // Return the time difference string
+  return `${daysDiff} days, ${hoursDiff} hours, ${minutesDiff} minutes, ${secondsDiff} seconds`;
 };
 
-export default function Home({ serverTime }) {
+export default function Home({serverTime}: HomeProps) {
   const router = useRouter();
   const [clientTime, setClientTime] = useState(new Date());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setClientTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
   const moveToTaskManager = () => {
     router.push("/tasks");
-  };
+  }
 
-  const serverDate = new Date(serverTime);
+  //Parse the server time
+  const parsedServerTime = new Date(serverTime);
 
-  const timeDiff = calculateTimeDifference(serverDate.getTime(), clientTime.getTime());
+  //Format the parsed server time
+  const formattedServerTime = parsedServerTime.toLocaleString('fr-FR', {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).replace(',', '').replace(/\//ig, "-");
+  const timeDifference = calculateTimeDifference(parsedServerTime, clientTime);
 
   return (
     <>
@@ -44,13 +62,19 @@ export default function Home({ serverTime }) {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
+          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
-            Server time: <span className="serverTime">{serverTime}</span>
+            Server time:{" "}
+            <span className="serverTime">{formattedServerTime}</span>
           </p>
+
+          {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff: <span className="serverTime">{timeDiff}</span>
+            Time diff:{" "}
+            <span className="serverTime">{timeDifference}</span>
           </p>
         </div>
+
         <div>
           <button onClick={moveToTaskManager}>Go to task manager</button>
         </div>
@@ -59,8 +83,9 @@ export default function Home({ serverTime }) {
   );
 }
 
-export async function getServerSideProps() {
-  const serverTime = new Date().toLocaleString();
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverTime = new Date().toISOString(); // Convert Date to string representation
+
   return {
     props: {
       serverTime,
